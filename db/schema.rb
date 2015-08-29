208ab33aca764ed291f38d9ee34356e201b9b1f2
banganga <alex_5454@list.ru>
2015-08-29 19:48:58 +0500

    db rollback and deleting articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821113644) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
