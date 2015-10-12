73287e4e5382d2728afa1659581cf866d41871bb
banganga <alex_5454@list.ru>
2015-10-12 14:23:40 +0500

    last changes pop index

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,21 @@
 class CommentsController < ApplicationController
 	def create
 		@post = Post.find(params[:post_id])
-		@comment = @post.comments.create(comment_params)
-			redirect_to post_path(@post)
+		if post
+			post.comments.create(comment_params)
+			flash[:success] = 'Thanks for comment!'
+			redirect_to post
+		else
+			flash[:danger] = 'Post not found'
+			redirect_to root_url
+		end
 	end
 	def destroy
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
-    @comment.destroy
-    redirect_to post_path(@post)
-  end
+	    @post = Post.find(params[:post_id])
+	    @comment = @post.comments.find(params[:id])
+	    @comment.destroy
+	    redirect_to post_path(@post)
+	end
 	private
 
 	def comment_params
