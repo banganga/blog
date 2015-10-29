7a9fb60de93cd1aaa6f921840cbf711233002c8d
Vladimir Evdokimov <evdokimov.vladimir@gmail.com>
2015-10-29 12:03:28 +0700

    Remove merge conflict leftovers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,22 @@
 class CommentsController < ApplicationController
+
 	def create
 		@post = Post.find(params[:post_id])
-<<<<<<< HEAD
-		@comment = @post.comments.create(comment_params)
-			redirect_to post_path(@post)
-		@comments.comments_count == comments_count + 10
-=======
-        @comment = @post.comments.create(comment_params)
-            redirect_to post_path(@post)
->>>>>>> popularity
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
 	end
+
 	def destroy
 	    @post = Post.find(params[:post_id])
 	    @comment = @post.comments.find(params[:id])
 	    @comment.destroy
 	    redirect_to post_path(@post)
 	end
+
 	private
 
 	def comment_params
-		params.require(:comment).permit(:commenter, :body)	
+		params.require(:comment).permit(:commenter, :body)
 	end
+
 end
