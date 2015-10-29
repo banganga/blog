7cf10af37314eabb0f588820a5f3ff967e152a4b
Vladimir Evdokimov <evdokimov.vladimir@gmail.com>
2015-10-29 12:00:41 +0700

    Seed admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+unless User.where(:id => 1).exists?
+  admin_password = "stone32"
+  user = User.new(
+    email: "admin@example.com",
+    password: admin_password,
+    password_confirmation: admin_password
+  )
+  user.id = 1
+  user.save!
+  puts "Admin user created: #{user.email} / #{admin_password}"
+end
